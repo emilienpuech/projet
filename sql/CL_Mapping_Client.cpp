ec48918f189781046f1f1a5b8fd6ab0eeb94091f
emilienpuech <emilienpuech@viacesi.fr>
2021-11-26 16:12:17 +0100

    update class mapp client

--- sql/CL_Mapping_Client.cpp
+++ sql/CL_Mapping_Client.cpp
@@ -12,23 +12,24 @@ System::String^ NS_Comp_Mappage::CL_Mapping_Client::Select(void)
 
 System::String^ NS_Comp_Mappage::CL_Mapping_Client::Insert(void)
 {
-		return "INSERT INTO [projetpoo].[dbo].[Client] ([Nom], [Prenom], [DateNaissance]) VALUES('" + this->nom + "','" + this->prenom + "','" + this->date_de_naissance + "');";
+	return "INSERT INTO [projetpoo].[dbo].[Client] ([Nom], [Prenom], [DateNaissance]) VALUES('" + this->nom + "','" + this->prenom + "','" + this->date_de_naissance + "');";
 }
 
 System::String^ NS_Comp_Mappage::CL_Mapping_Client::Delete(void)
 {
-	return "DELETE FROM [projetpoo].[dbo].[Client] WHERE numeroClient = "+ this->Id +";";
+	return "DELETE FROM [projetpoo].[dbo].[Client] WHERE numeroClient = " + this->Id + ";";
 }
 
 System::String^ NS_Comp_Mappage::CL_Mapping_Client::Update(void)
 {
-	return;
+	return "UPDATE[projetpoo].[dbo].[Client] SET Nom = " + this->nom + ", Prenom = " + this->prenom + ", DateNaissance = '' = " + this->nom + " WHERE numeroClient = " + this->Id + "; ";
 }
 
+
 // tout nos Setter NOM PRENOM DATE DE NAISSANCE
-void NS_Comp_Mappage::CL_Mapping_Client::setNom(System::String^ nom){ this->nom = nom; }
-void NS_Comp_Mappage::CL_Mapping_Client::setPrenom(System::String^ prenom){ this->prenom = prenom; }
-void NS_Comp_Mappage::CL_Mapping_Client::setdate_de_naissance(System::String^ date_de_naissance){ this->date_de_naissance = date_de_naissance; }
+void NS_Comp_Mappage::CL_Mapping_Client::setNom(System::String^ nom) { this->nom = nom; }
+void NS_Comp_Mappage::CL_Mapping_Client::setPrenom(System::String^ prenom) { this->prenom = prenom; }
+void NS_Comp_Mappage::CL_Mapping_Client::setdate_de_naissance(System::String^ date_de_naissance) { this->date_de_naissance = date_de_naissance; }
 
 
 
