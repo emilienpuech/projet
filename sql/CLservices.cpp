6826bfd52bdc7302a386f6f364f114420e9bd22d
emilienpuech <emilienpuech@viacesi.fr>
2021-11-27 12:55:05 +0100

    modif service pour la map client

--- sql/CLservices.cpp
+++ sql/CLservices.cpp
@@ -6,14 +6,16 @@ NS_Comp_Svc::CLservices::CLservices(void)
 {
 	this->oCad = gcnew NS_Comp_Data::CLcad();
 	this->oMappTB = gcnew NS_Comp_Mappage::CLmapTB();
+	this->Mapp_client = gcnew NS_Comp_Mappage::CL_Mapping_Client();
 }
-System::Data::DataSet^ NS_Comp_Svc::CLservices::selectionnerToutesLesPersonnes(System::String^ dataTableName)
+System::Data::DataSet^ NS_Comp_Svc::CLservices::select_tout_nos_client(System::String^ dataTableName)
 {
 	System::String^ sql;
 
-	sql = this->oMappTB->Select();
+	sql = this->Mapp_client->Select();
 	return this->oCad->getRows(sql, dataTableName);
 }
+
 void NS_Comp_Svc::CLservices::ajouterUnePersonne(System::String^ nom, System::String^ prenom)
 {
 	System::String^ sql;
@@ -24,3 +26,35 @@ void NS_Comp_Svc::CLservices::ajouterUnePersonne(System::String^ nom, System::St
 
 	this->oCad->actionRows(sql);
 }
+
+
+
+
+/*
+*	Exemple méthode de service du Select Execute Query 
+
+System::Data::DataSet^ NS_Comp_Svc::CLservices::METHODE SERVICE(System::String^ dataTableName)
+{
+	System::String^ sql;
+
+	sql = this->CLASSE DE MAPPAGE->Select();
+	return this->oCad->getRows(sql, dataTableName);
+}
+
+
+
+
+// Exemple méthode de service du Execute non query
+
+void NS_Comp_Svc::CLservices::ajouterUnePersonne(System::String^ nom, System::String^ prenom)
+{
+	System::String^ sql;
+
+	this->oMappTB->setNom(nom);
+	this->oMappTB->setPrenom(prenom);
+	sql = this->oMappTB->Insert();
+
+	this->oCad->actionRows(sql);
+}
+
+*/
--- sql/CLservices.h
+++ sql/CLservices.h
@@ -14,17 +14,19 @@ namespace NS_Comp_Svc
 								   
 		
 		// Classe de mappage 
-		NS_Comp_Mappage::CLmapTB^ oMappTB; 
+		NS_Comp_Mappage::CLmapTB^ oMappTB;
+		NS_Comp_Mappage::CL_Mapping_Client^ Mapp_client;
 
 	
 	
 	public:
 		CLservices(void);
-		System::Data::DataSet^ selectionnerToutesLesPersonnes(System::String^);
+		System::Data::DataSet^ select_tout_nos_client(System::String^);
 		void ajouterUnePersonne(System::String^, System::String^);
 		
 
 		// Nous ajoutons nous fonctions de service qui utilisera nous méthode du CAD et de nos classe de mappage
+		System::Data::DataSet^ sel (System::String^);
 
 
 	};
@@ -33,3 +35,4 @@ namespace NS_Comp_Svc
 
 
 }
+ 
