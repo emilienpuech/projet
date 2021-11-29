2e1b3b97fba5b359c365051282948e5c364d8c0e
emilienpuech <emilienpuech@viacesi.fr>
2021-11-29 22:59:21 +0100

    modif class remise

--- sql/CL_Mapping_Remise.cpp
+++ sql/CL_Mapping_Remise.cpp
@@ -2,24 +2,24 @@
 
 System::String^ NS_Comp_Mappage::CL_Mapping_Remise::Select(void)
 {
-    return"SELECT [idPaiement],[pourcentageRemise] FROM [projetpoo].[dbo].[Remise];";
+    return"";
 }
 
 System::String^ NS_Comp_Mappage::CL_Mapping_Remise::Insert(void)
 {
-    return "INSERT INTO [projetpoo].[dbo].[ID_Paiement] ( [ref_Commande) VALUES('" + this->ID_paiment + "');";
+    return "";
 }
 
 System::String^ NS_Comp_Mappage::CL_Mapping_Remise::Delete(void)
 {
-    return "DELETE FROM [projetpoo].[dbo].[ref_Commande] WHERE numeroClient = '" + this->id + "';";
+    return "";
 }
 
 System::String^ NS_Comp_Mappage::CL_Mapping_Remise::Update(void)
 {
-    return "UPDATE[projetpoo].[dbo].[ID_paiement] SET ref_Commande ='" + this->ref_Commande + "' WHERE ID_Paiement = '" + this->ID_paiement + "'; ";
+    return "";
 }
 
 void NS_Comp_Mappage::CL_Mapping_Remise::setid(int id) { this->id = id; }
-void NS_Comp_Mappage::CL_Mapping_Remise::setNom_Taxe(System::String^ nom_Taxe) { this->nom_Taxe = nom_Taxe; }
-void NS_Comp_Mappage::CL_Mapping_Remise::setPourcentageTaxe(float pourcentageTaxe) { this->pourcentageTaxe = pourcentageTaxe; }
+void NS_Comp_Mappage::CL_Mapping_Remise::setnomremise(System::String^ nomRemise) { this->nomRemise = nomRemise; }
+void NS_Comp_Mappage::CL_Mapping_Remise::setpourcentageRemise(float pourcentageRemise) { this->pourcentageRemise = pourcentageRemise; }
