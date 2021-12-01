13ae791b4111c578a1317eeb75417556b3ebfc04
OwnerOwl <91188521+OwnerOwl@users.noreply.github.com>
2021-12-01 14:51:46 +0100

    modif

--- sql/CL_Mapping_Posseder.cpp
+++ sql/CL_Mapping_Posseder.cpp
@@ -5,21 +5,22 @@ System::String^ NS_Comp_Mappage::CL_Mapping_Posseder::Select(void)
 	return"SELECT [idAdresse],[[typeAdresse]],[numeroClient] FROM [projetpoo].[dbo].[Posseder];";
 }
 
-/*System::String^ NS_Comp_Mappage::CL_Mapping_Paiement::Insert(void)
+System::String^ NS_Comp_Mappage::CL_Mapping_Posseder::Insert(void)
 {
-	return "INSERT INTO [projetpoo].[dbo].[Posseder] ( [type]) VALUES('" + this->moyen_de_paiment + "');";
+	return "INSERT INTO [projetpoo].[dbo].[Posseder] ( [type]) VALUES('" + this->Type_Adresse + "');";
 }
 
-System::String^ NS_Comp_Mappage::CL_Mapping_Paiement::Delete(void)
+System::String^ NS_Comp_Mappage::CL_Mapping_Posseder::Delete(void)
 {
-	return "DELETE FROM [projetpoo].[dbo].[ModePaiement] WHERE numeroClient = '" + this->id + "';";
+	return "DELETE FROM [projetpoo].[dbo].[Posseder] WHERE idAdresse = '" + this->idAdresse + "';";
 }
 
-System::String^ NS_Comp_Mappage::CL_Mapping_Paiement::Update(void)
+System::String^ NS_Comp_Mappage::CL_Mapping_Posseder::Update(void)
 {
-	return "UPDATE[projetpoo].[dbo].[ModePaiement] SET MoyenPaiement ='" + this->moyen_de_paiment + "' WHERE IDMoyenPaiement = '" + this->id + "'; ";
+	return "UPDATE[projetpoo].[dbo].[ModePaiement] SET Type_Adresse ='" + this->Type_Adresse + "' WHERE idAdresse = '" + this->idAdresse + "'; ";
 }
 
 
-void NS_Comp_Mappage::CL_Mapping_Paiement::setid(int id) { this->id = id; }
-void NS_Comp_Mappage::CL_Mapping_Paiement::setmoyen_de_paiment(System::String^ moyen_de_paiment) { this->moyen_de_paiment = moyen_de_paiment; }*/
+void NS_Comp_Mappage::CL_Mapping_Posseder::setid(int id) { this->idAdresse = idAdresse; };
+void NS_Comp_Mappage::CL_Mapping_Posseder::settypeAdresse(System::String^ Type_Adresse) { this->Type_Adresse = Type_Adresse; };
+void NS_Comp_Mappage::CL_Mapping_Posseder::setnumeroClient(float numeroClient) { this->numeroClient = numeroClient; };
