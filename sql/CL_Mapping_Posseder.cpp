54bb2f26fe033b37bcfcfd802a88a7e95eff2587
OwnerOwl <91188521+OwnerOwl@users.noreply.github.com>
2021-12-02 09:44:40 +0100

    jhk

--- sql/CL_Mapping_Posseder.cpp
+++ sql/CL_Mapping_Posseder.cpp
@@ -5,17 +5,17 @@ System::String^ NS_Comp_Mappage::CL_Mapping_Posseder::Select(void)
 	return"SELECT [idAdresse],[[typeAdresse]],[numeroClient] FROM [projetpoo].[dbo].[Posseder];";
 }
 
-System::String^ NS_Comp_Mappage::CL_Mapping_Posseder::Insert(void)
+/*System::String^ NS_Comp_Mappage::CL_Mapping_Paiement::Insert(void)
 {
-	return "INSERT INTO [projetpoo].[dbo].[Posseder] ( [type]) VALUES('" + this->Type_Adresse + "');";
+	return "INSERT INTO [projetpoo].[dbo].[Posseder] ( [type]) VALUES('" + this->moyen_de_paiment + "');";
 }
 
-System::String^ NS_Comp_Mappage::CL_Mapping_Posseder::Delete(void)
+System::String^ NS_Comp_Mappage::CL_Mapping_Paiement::Delete(void)
 {
-	return "DELETE FROM [projetpoo].[dbo].[Posseder] WHERE idAdresse = '" + this->idAdresse + "';";
+	return "DELETE FROM [projetpoo].[dbo].[ModePaiement] WHERE numeroClient = '" + this->id + "';";
 }
 
-System::String^ NS_Comp_Mappage::CL_Mapping_Posseder::Update(void)
+System::String^ NS_Comp_Mappage::CL_Mapping_Paiement::Update(void)
 {
 	return "UPDATE[projetpoo].[dbo].[POsseder] SET TypeAdresse ='" + this->Type_Adresse + "' WHERE idAdresse = '" + this->idAdresse + "'; ";
 }
